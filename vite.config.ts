ff06db7c6fb5e27c44eea5f0c190842255251d84
Dinuka Ekanayake <79810886+DinukaEk@users.noreply.github.com>
2026-04-14 19:45:09 +0530

    fix v0.3

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  // Replace 'portfolio-new' with your exact GitHub repo name
   base: '/portfolio-new/',
-}))
+})
